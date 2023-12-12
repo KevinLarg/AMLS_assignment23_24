e5cf41bd26eb7b80e034fdf20b972e7c7e0d7e0d
KevinLagr <kevin6lagrange@gmail.com>
2023-12-13 02:18:29 +0800

    First Try Github Setting-up

---
+++ main.py
@@ -0,0 +1,12 @@
+# ALMS 1 ELEC0134
+# Term 1 Final Assignment
+# By SN 23043422, from 13/12/2023 to ??/01/2024
+
+# This file aim to address all main code parts used when solving task A and B described in the task sheet;
+
+# Environment: Python 3.12.0
+
+# Library installed (as the order to be used): 
+
+# Task A
+
